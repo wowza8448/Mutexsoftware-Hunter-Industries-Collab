90becf69583d4049b22330447cb4777710137e1c
wowza8448 <71194678+wowza8448@users.noreply.github.com>
2022-02-07 20:38:41 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkhtmlview import HTMLLabel
+import webbrowser
+import tkgen.gengui
+
+root = Tk()
+root.title('TBD')
+root.geometry("600x200")
+
+#file = open("sample.json")
+#json_dictionary = json.load(file)
+#file.close()
+
+
+
+
+root.mainloop()
