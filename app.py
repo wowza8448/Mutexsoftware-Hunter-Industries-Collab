99c1639ddff2835de2245d7a9c6d302b449b8407
wowza8448 <caste107@cougars.csusm.edu>
2022-03-10 00:47:52 +0000

    Working First section some bugs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, g, jsonify
+from flask import Flask, render_template, request, redirect, url_for, g, jsonify, session, json
 #from flask_json import FlaskJSON, JsonError, json_response, as_json
 
 import sys
@@ -12,7 +12,7 @@ from db_con import get_db_instance, get_db
 
 app = Flask(__name__)
 #FlaskJSON(app)
-
+app.secret_key = "super secret key"
 
 @app.route('/')
 def index():
@@ -20,21 +20,31 @@ def index():
 
 @app.route('/get_id', methods=['GET', 'POST'])
 def get_id():
-    global_db_con = get_db()
-    id = request.form['ID']
-    print("Obtained id: " + id)
-    cur = global_db_con.cursor()
-    sql = """SELECT * FROM keys;"""
-    cur.execute(sql)
-    match = cur.fetchall()
-    for row in match:
-        print(row[0])
-        if id == row[0]:
-            print("Match was found")
-            new = row[1]
-            return render_template('index.html', pass_back = "hello")
-    print("Match was never found")
-    return "Error no match found"
+    if request.method == 'POST':
+        global_db_con = get_db()
+        id = request.form['ID']
+        print("Obtained id: " + id)
+        cur = global_db_con.cursor()
+        sql = """SELECT * FROM keys;"""
+        cur.execute(sql)
+        match = cur.fetchall()
+        for row in match:
+            if id == row[0]:
+                print("Match was found")
+                session['pass_back'] = row[1]
+                new = row[1]
+                return redirect(url_for('pass_new', data = new))
+            if id != row[0]:
+                session['pass_back'] = "Error Invalid"
+        session['pass_back'] = "Error Invalid"
+        return "No Match Was Found"
+    return "An error has occured"
+
+@app.route('/pass_new')
+def pass_new():
+    data = session['pass_back']
+    return data
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
